e1ff3e5cc392109034f73607017d3a739ef2f489
lmzacarias <luismiguelzacarias@outlook.com>
2021-10-10 23:18:10 -0500

    edit init

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,7 @@ const Hero = (props) => (
           
         </SectionTitle>
         <SectionText>
-        Mi nombre es Luis Miguel Zacarias si estás aquí es porque al igual que a mi te gusta la Arquitectura y necesitas ayuda para dar tus primeros pasos, es por ello que se creo esta página para que entre todos podamos ayudarnos y crear conocimiento.
+        Mi nombre es Luis Miguel Zacarias si estás aquí es porque al igual que a mi te gusta la Arquitectura y necesitas ayuda para dar tus primeros pasos y por tal razón comenzo está página para hacer una comunidad y entre todos podamos ayudarnos y crear conocimiento.
         </SectionText>
         <Button onClick={props.handleClick}>Learn More</Button>
       </LeftSection>
